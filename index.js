c571af088b011de21809b7e4bbd92d367ecbfec4
Dan Laush <dan.laush@gmail.com>
2019-03-02 16:15:19 +1100

    Commit dist folder, add index.html landing page

--- index.js
+++ index.js
@@ -1,53 +1,63 @@
 (function() {
+  const runURL = 'https://app.10000ft.com/me/tracker';
 
-  const runURL = 'https://app.10000ft.com/me/tracker'
-
-  if(window.location.href !== runURL) {
-    alert(`The 10k billable bookmarklet only runs on ${runURL}`)
-    return
+  if (window.location.href !== runURL) {
+    alert(`The 10k billable bookmarklet only runs on ${runURL}`);
+    return;
   }
 
   const data = [
-    { // monday
+    {
+      // monday
       billable: 0,
       unbillable: 0
     },
-    { // tuesday
+    {
+      // tuesday
       billable: 0,
       unbillable: 0
     },
-    { // wednesday
+    {
+      // wednesday
       billable: 0,
       unbillable: 0
     },
-    { // thursday
+    {
+      // thursday
       billable: 0,
       unbillable: 0
     },
-    { // friday
+    {
+      // friday
       billable: 0,
       unbillable: 0
     }
-  ]
+  ];
 
-  const projects = document.querySelectorAll('.tk-time-tracker-row')
+  const projects = document.querySelectorAll('.tk-time-tracker-row');
   // console.log('projects', projects);
 
-  for(i=0;i<projects.length;++i) {
-    const days = projects[i].querySelectorAll('.tk-time-tracker-cel')
-    
-    for(j=0;j<days.length;++j) {
+  for (i = 0; i < projects.length; ++i) {
+    const days = projects[i].querySelectorAll('.tk-time-tracker-cel');
+
+    for (j = 0; j < days.length; ++j) {
       // console.log('days', days[j].classList);
-      if(!days[j].classList.contains('empty')) {
-        const hours = parseFloat(days[j].querySelector('.tk-hours').textContent)
+      if (!days[j].classList.contains('empty')) {
+        const hours = parseFloat(
+          days[j].querySelector('.tk-hours').textContent
+        );
         // console.log('hours', hours)
-        if(days[j].classList.contains('grad-purple')) {
+        if (days[j].classList.contains('grad-purple')) {
           // console.log(`unbillable work ${hours} hours`)
-          data[j].unbillable = Number(parseFloat(data[j].unbillable + hours).toFixed(1))
+          data[j].unbillable = Number(
+            parseFloat(data[j].unbillable + hours).toFixed(1)
+          );
         }
-        if(days[j].classList.contains('grad-blue')) {
+        if (days[j].classList.contains('grad-blue')) {
           // console.log(`billable work ${hours} hours`)
-          data[j].billable = Number(parseFloat(data[j].billable + hours).toFixed(1))
+          data[j].billable = Number(
+            parseFloat(data[j].billable + hours).toFixed(1)
+          );
         }
       }
     }
@@ -55,41 +65,41 @@
 
   const output = `\
 Billable this week: ${billablePerWeek(data)}
-Mon: ${billablePerDay(data[0])}%;\
-Tue: ${billablePerDay(data[1])}%;\
-Wed: ${billablePerDay(data[2])}%;\
-Thu ${billablePerDay(data[3])}%;\
+Mon: ${billablePerDay(data[0])}%; \
+Tue: ${billablePerDay(data[1])}%; \
+Wed: ${billablePerDay(data[2])}%; \
+Thu ${billablePerDay(data[3])}%; \
 Fri ${billablePerDay(data[4])}%;\
-`
+`;
 
   function billablePerWeek(week) {
-    let billable = 0
-    let total = 0
-    for(i=0;i<week.length;++i) {
-      billable = Number((billable + week[i].billable).toFixed(1))
-      total = Number((total + (week[i].billable + week[i].unbillable)).toFixed(1))
+    let billable = 0;
+    let total = 0;
+    for (i = 0; i < week.length; ++i) {
+      billable = Number((billable + week[i].billable).toFixed(1));
+      total = Number(
+        (total + (week[i].billable + week[i].unbillable)).toFixed(1)
+      );
     }
 
-    return `${fractionToPercent(billable, total)}% (${billable} / ${total})`
+    return `${fractionToPercent(billable, total)}% (${billable} / ${total})`;
   }
 
   function billablePerDay(day) {
-    return fractionToPercent(day.billable, day.billable + day.unbillable)
+    return fractionToPercent(day.billable, day.billable + day.unbillable);
   }
 
   function fractionToPercent(top, bottom) {
-    if(!top || !bottom) return 0
-    return (top / bottom * 100).toFixed(0)
+    if (!top || !bottom) return 0;
+    return ((top / bottom) * 100).toFixed(0);
   }
 
   alert(output);
-
 })();
 
 // Output:
 // Billable this week: 85% (34 / 36)
 // Mon: 85%; Tue: 80%; Wed: 90%; Thu 85%; Fri 85%;
-
 
 // .time-entry (dates/hours grid)
 // .tk-time-tracker-row (project)
